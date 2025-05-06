a04528f1eeb63e6c80943471bd879efa615049be
IAmSpring <11973697+IAmSpring@users.noreply.github.com>
2025-05-06 16:56:59 +0000

    Initial commit of Prosper Health App Architecture

--- apps/web/src/utils/trpc.ts
+++ apps/web/src/utils/trpc.ts
@@ -1,5 +1,5 @@
-import { createTRPCReact, httpBatchLink } from '@trpc/react-query';
-import type { AppRouter } from '../../../backend/src/routers';
+import { createTRPCReact, httpBatchLink, loggerLink } from "@trpc/react-query";
+import type { AppRouter } from "../../../backend/src/routers";
 
 // Create tRPC React client
 export const trpc = createTRPCReact<AppRouter>();
@@ -7,8 +7,9 @@ export const trpc = createTRPCReact<AppRouter>();
 // Create tRPC client
 export const trpcClient = trpc.createClient({
   links: [
+    loggerLink(),
     httpBatchLink({
-      url: 'http://localhost:3001/trpc',
+      url: "http://localhost:3001/trpc",
     }),
   ],
-}); 
+});
